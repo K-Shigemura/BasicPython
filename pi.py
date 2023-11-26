ca913726221a962a4dd071035e3bad91cf1fa9c1
K-Shigemura <k.shigemura1013@gmail.com>
2023-11-26 21:28:07 +0900

    pi.pyに修正を加えました

--- pi.py
+++ pi.py
@@ -10,9 +10,9 @@ words = [word.strip('.') for word in words]
 words = [word.strip('\n') for word in words]
 words = [word for word in words if word != '']
 print(words)
-numberofletters = [len(word) for word in words]
+number_of_letters = [len(word) for word in words]
 pi=''
-for number in numberofletters:
+for number in number_of_letters:
     pi += str(number)
 print(pi)
 
--- quadratic_formula.py
+++ quadratic_formula.py
@@ -1,7 +1,6 @@
 coefficients = [(1, -6, 9), (1, 2, -8), (8, -6, -35), (1, 0, 1)]
 
 for a, b, c in coefficients:
-    # TODO
     x1 = (-b+((b**2-4*a*c)**0.5))/(2*a)
     x2 = (-b-((b**2-4*a*c)**0.5))/(2*a)
     print(f"x={x1}, {x2} (a={a}, b={b}, c={c})")
