14c73b64d2bd84822409a39b16278fd58ab3b491
K-Shigemura <k.shigemura1013@gmail.com>
2023-11-27 00:20:42 +0900

    関数の解答

--- euclid.py
+++ euclid.py
@@ -1,4 +1,36 @@
+from math import pi
+import random
+def euclid(a, b):
+    q = a // b
+    r = a % b
+    if r == 0:
+        return b
+    else:
+        return euclid(b, r)
+
+def muttualy_prime(a, b):
+    return euclid(a, b) == 1
+
 a = input("a の値を入力: ")
 b = input("b の値を入力: ")
+print(euclid(int(a), int(b)))
 
-# TODO
+#エクストラ問題
+def random_number():
+    return int(1000000 * random.random())
+
+def tuple_random_number():
+    tuples = list()
+    for x in range(100000):
+        a = random_number()
+        b = random_number()
+        tuples.append((a, b))
+    return tuples
+
+tuples = tuple_random_number()
+count = 0
+for a, b in tuples:
+    if muttualy_prime(a, b):
+        count += 1
+print(count / len(tuples))
+print(f"6/π^2との差: {abs(count / len(tuples) - 6 / pi**2)}")
